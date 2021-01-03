b5dc45a397fafcf1b3bd197b64a8e4b942b81d5f
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-01-04 00:57:54 +0900

    Use HashRouter

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
+import {HashRouter, Redirect, Route, Switch} from "react-router-dom";
 import MainPage from "./components/MainPage";
 import MnistPage from "./components/MnistPage";
 
 export const Routes: React.FC = () => (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
         <Switch>
             <Route exact path="/" component={MainPage}/>
             <Route exact path="/mnist" component={MnistPage}/>
             <Redirect from="*" to="/"/>
         </Switch>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 export default Routes;
