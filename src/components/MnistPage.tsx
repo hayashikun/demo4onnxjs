6bd1b9ac2453b219d072d3fc13b884f4b7f0a0ac
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-01-03 16:15:44 +0900

    Add evalation

--- src/components/MnistPage.tsx
+++ src/components/MnistPage.tsx
@@ -1,26 +1,53 @@
-import React, {useState} from "react";
-import {InferenceSession} from "onnxjs"
+import React, {useRef, useState} from "react";
+import {InferenceSession, Tensor} from "onnxjs"
+import DrawableCanvas from "./DrawableCanvas";
 
 
 const MnistPage: React.FC = () => {
+    const canvasRef: React.MutableRefObject<DrawableCanvas | null> = useRef(null);
     const [modelLoaded, setModelLoaded] = useState(false);
     const [backend, setBackend] = useState("cpu");
     const [version, setVersion] = useState(9);
-    let session = new InferenceSession({backendHint: "cpu"})
+    const [inferenceSession, setInferenceSession]: [InferenceSession | null, any] = useState(null)
+    const [probabilities, setProbabilities] = useState(new Float32Array(10))
 
     const loadModel = async () => {
         setModelLoaded(false);
-        session = new InferenceSession({backendHint: backend});
+        let session = new InferenceSession({backendHint: backend});
         try {
             await session.loadModel(`/models/mnist_1_v${version}.onnx`);
             setModelLoaded(true);
+            setInferenceSession(session);
         } catch (e) {
             console.log(e)
         }
     }
 
+    const evaluate = async () => {
+        let canvas = canvasRef.current;
+        if (canvas == null || inferenceSession == null) {
+            return;
+        }
+        const data = canvas.getImageData();
+        if (data == null) {
+            return;
+        }
+        console.log(data.map(d => d * 2 - 1))
+        const tensor = new Tensor(data.map(d => d * 2 - 1), "float32", [1, 28 * 28]);
+        const result = await inferenceSession!.run([tensor]);
+        setProbabilities(result.values().next().value.data);
+    }
+
+    const clearCanvas = () => {
+        let canvas = canvasRef.current;
+        if (canvas == null) {
+            return;
+        }
+        canvas.clearCanvas();
+    }
+
     return (
-        <div>
+        <div style={{margin: "24px"}}>
             <h1>MNIST</h1>
 
             <div>
@@ -55,8 +82,29 @@ const MnistPage: React.FC = () => {
                 {modelLoaded && <span>({backend}, {version})</span>}
             </div>
 
+            <DrawableCanvas
+                ref={canvasRef}
+                style={{border: "1px solid black"}}
+                displayHeight={300}
+                displayWidth={300}
+                dataHeight={28}
+                dataWidth={28}
+                lineWidth={10}
+            />
+            <button onClick={evaluate}>Eval</button>
+            <button onClick={clearCanvas}>Clear</button>
+
+            <ul>{
+                (() => {
+                    const ans = Array.from({length: 10}, (v, k) => probabilities[k]);
+                    const idx = ans.indexOf(Math.max(...ans))
+                    return ans.map((a, i) =>
+                        <li key={i} style={{color: i === idx ? "red" : "black"}}>{i}: {a}</li>
+                    )
+                })()
+            }</ul>
         </div>
     );
-};
+}
 
 export default MnistPage;
