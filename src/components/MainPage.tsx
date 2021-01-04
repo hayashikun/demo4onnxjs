d5f1ee5ece0bcafaa8873cbef71a636cbf3c8795
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-01-04 16:16:13 +0900

    Add links

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -8,6 +8,22 @@ const MainPage: React.FC = () => {
             <ul>
                 <li><Link to="/mnist">MNIST</Link></li>
             </ul>
+
+            <ul>
+                <li><Link to="https://github.com/hayashikun/demo4onnxjs">hayashikun/demo4onnxjs</Link></li>
+                <li>
+                    <Link to="https://github.com/microsoft/onnxjs">microsoft/onnxjs</Link>
+                    <ul>
+                        <li><Link to="https://github.com/microsoft/onnxjs/blob/master/docs/operators.md">operators.md</Link></li>
+                    </ul>
+                </li>
+                <li>
+                    <Link to="https://github.com/onnx/onnx">onnx/onnx</Link>
+                    <ul>
+                        <li><Link to="https://github.com/onnx/onnx/blob/rel-1.2.3/docs/Operators.md">Operators.md</Link></li>
+                    </ul>
+                </li>
+            </ul>
         </div>
     );
 };
